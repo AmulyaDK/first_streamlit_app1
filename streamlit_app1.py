bdb8a20cd88467b3bd932d99b9d4574f278da3c0
AmulyaDK <160099986+AmulyaDK@users.noreply.github.com>
2024-02-15 13:57:57 +0530

    Update streamlit_app1.py
    changed the header

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My new healthy dinner')
-streamlit.text('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Dosa and Idli')
 streamlit.text('Juice and fruits')
 streamlit.text('egg and bread')
